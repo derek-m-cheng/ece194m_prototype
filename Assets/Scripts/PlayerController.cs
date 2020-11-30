631e9461e384ac272ef71290fa6b6a01cf69d9f8
Derek <dcheng@umail.ucsb.edu>
2020-11-29 23:19:36 -0800

    prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public float speed = 2.0f;
+    Rigidbody2D rigidbody2d;
+    public float charger;
+    public bool discharge;
+    public float jumpForce;
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey (KeyCode.A))
+        {
+            rigidbody2d.velocity = new Vector2(speed * -1, rigidbody2d.velocity.y);
+        }
+        if (Input.GetKey (KeyCode.D))
+        {
+            rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+        }
+        // On (discharge == true)
+        if (discharge)
+        {
+            // Set jump force and give new velocity
+            jumpForce = 10 * charger;
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
+
+            // Reset discharge and charger values
+            discharge = false;
+            charger = 0f;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            charger += UnityEngine.Time.deltaTime;
+        }
+
+        // On release, set the boolean 'discharge' to true.
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            discharge = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+    float enrageTime = 55f;
+
+    [SerializeField] Text countdownText;
+    [SerializeField] Text enrageText;
+    [SerializeField] Text enrageTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        enrageText.gameObject.SetActive(false);
+        enrageTimer.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * UnityEngine.Time.smoothDeltaTime;
+        countdownText.text = currentTime.ToString("0");
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+        if (currentTime <= startingTime/2)
+        {
+            countdownText.color = Color.red;
+        }
+        if (currentTime == 0)
+        {
+
+            countdownText.gameObject.SetActive(false);
+            enrageText.gameObject.SetActive(true);
+            //enrageTimer.gameObject.SetActive(true);
+            do
+            {
+                enrageTime -= 1 * UnityEngine.Time.smoothDeltaTime;
+                enrageTimer.text = enrageTime.ToString("0");
+            } while (enrageTime >= 0);
+        }
+    }
+
+}
