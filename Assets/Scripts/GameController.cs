8a4f715749f94f1fce561085407c14635a326c52
Derek <dcheng@umail.ucsb.edu>
2020-12-18 21:13:29 -0800

    release

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject target;
+    public Text timerText;
+    private float secondsCount;
+    public Text winText;
+    public Text playAgainText;
+    private bool win;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        winText.gameObject.SetActive(false);
+        playAgainText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateTimerUI();
+        if (Input.GetKey(KeyCode.Escape)) {
+            doExitGame();
+        }
+    }
+
+    //call this on update
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        if (win == false) {
+            secondsCount += Time.deltaTime;
+            timerText.text = "CURRENT RUN'S TIME: " + (int)secondsCount + "s";
+        }
+        if (target)
+        {
+      
+
+           if (target.transform.position.x < -1.2 && target.transform.position.y > 56)
+            {
+                win = true;
+                winText.gameObject.SetActive(true);
+                playAgainText.gameObject.SetActive(true);
+                winText.fontSize = 70;
+                winText.text = "YOU WON IN " + (int)secondsCount + "s";
+                playAgainText.fontSize = 70;
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+
+        }
+
+    }
+
+    void doExitGame()
+    {
+        Application.Quit();
     }
 }
--- Assets/Scripts/NemesisController.cs
+++ Assets/Scripts/NemesisController.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class NemesisController : MonoBehaviour
 {
     public float currentTime = 0f;
-    float startingTime = 30f;
+    float startingTime = 100f;
+    public GameObject target;
 
     [SerializeField] Text nemesisSpawnText;
     [SerializeField] Text nemesisEnrageText;
+    [SerializeField] Text youLose;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,7 @@ public class NemesisController : MonoBehaviour
         currentTime = startingTime;
         nemesisSpawnText.fontSize = 1;
         nemesisEnrageText.fontSize = 1;
+        youLose.gameObject.SetActive(false);
 
     }
 
@@ -25,7 +29,7 @@ public class NemesisController : MonoBehaviour
     {
         currentTime -= 1 * UnityEngine.Time.smoothDeltaTime;
         nemesisEnrageText.text = currentTime.ToString("0");
-        if (currentTime <= 10)
+        if (currentTime <= 80)
         {
             nemesisSpawnText.fontSize = 30;
             nemesisEnrageText.fontSize = 30;
@@ -34,6 +38,17 @@ public class NemesisController : MonoBehaviour
         if(currentTime <= 0)
         {
             currentTime = 0;
+            nemesisEnrageText.fontSize = 1;
+            nemesisSpawnText.fontSize = 1;
+            Destroy(target);
+            youLose.gameObject.SetActive(true);
+            youLose.fontSize = 70;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+
         }
         if (currentTime <= startingTime / 2)
         {
